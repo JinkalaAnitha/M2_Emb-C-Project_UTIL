efae11f0b00973904ee51cbca7bf0a7db4239204
JinkalaAnitha <101356849+JinkalaAnitha@users.noreply.github.com>
2022-04-15 00:57:05 +0530

    Create Setup.c

---
+++ 3_Implementation/Src/Setup.c
@@ -0,0 +1,8 @@
+void setup() 
+{
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  Serial.println("Put your card to the reader...");
+  Serial.println();
+}
